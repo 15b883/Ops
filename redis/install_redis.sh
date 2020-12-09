8050186d882e1f262bb97c42d23d1f5b6d69b4aa
15b883 <syavingc.s@gmail.com>
2020-12-09 14:47:08 +0800

    更新redis

--- redis/install_redis.sh
+++ redis/install_redis.sh
@@ -1,22 +1,17 @@
 ## redis
 ## https://download.redis.io/releases/redis-6.0.8.tar.gz
 [ ! -d /opt/packages ]; mkdir -p /opt/packages
-[ ! -d /usr/local/redis ]; mkdir -p /usr/local/redis
-echo -n > /var/logs/redis/redis.log
+#[ ! -d /usr/local/redis ]; mkdir -p /usr/local/redis
+[ ! -d /var/log/redis ]; mkdir -p /var/log/redis
+cat /dev/null > /var/log/redis/redis.log
 
 ##
 cd /opt/packages
 wget http://download.redis.io/releases/redis-6.0.9.tar.gz
-tar xf redis-6.0.9.tar.gz
+tar xf redis-6.0.9.tar.gz -C /usr/local
 cd redis-6.0.9
 make
-make install PREFIX=/usr/local/redis
-##
-vim /data/server/redis/redis.conf
-daemonize  yes
-logfile "/var/logs/redis/redis.log" 
-#save 900 1
-#save 300 10
-#save 60 10000
-/data/server/redis/src/redis-server
+make install 
 
+## 
+netstat -ntpl |grep redis
