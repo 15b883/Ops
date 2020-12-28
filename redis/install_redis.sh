b0cbdc98bcd694a67172a46951bbda39782e78a2
15b883 <syavingc.s@gmail.com>
2020-12-28 15:16:03 +0800

    更新redis安装脚本

--- redis/install_redis.sh
+++ redis/install_redis.sh
@@ -1,5 +1,5 @@
 ## redis
-## https://download.redis.io/releases/redis-6.0.8.tar.gz
+## https://download.redis.io/releases/
 [ ! -d /opt/packages ]; mkdir -p /opt/packages
 #[ ! -d /usr/local/redis ]; mkdir -p /usr/local/redis
 [ ! -d /var/log/redis ]; mkdir -p /var/log/redis
@@ -18,9 +18,10 @@ echo "source /opt/rh/devtoolset-9/enable" >>/etc/profile
 cd /opt/packages
 wget http://download.redis.io/releases/redis-6.0.9.tar.gz
 tar xf redis-6.0.9.tar.gz -C /usr/local
-cd redis-6.0.9
+cd /usr/local/
+mv redis-6.0.9 redis
+
 make
 make install 
 
-## 
-netstat -ntpl |grep redis
+# redis.service 
